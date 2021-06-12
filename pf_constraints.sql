58da8772200974ce22986df317e833f135765c89
elangaar <mikolaj.kowal@yahoo.pl>
2021-06-12 10:25:06 +0200

    Chang the database model and table constraints

--- pf_constraints.sql
+++ pf_constraints.sql
@@ -1,23 +1,39 @@
+ALTER TABLE expenses
+  ADD CONSTRAINT exp_cat_exp_fk FOREIGN KEY ( exp_cat )
+    REFERENCES expense_categories ( exp_cat_id )
+    ON DELETE CASCADE;
+
 ALTER TABLE expense_categories
-    ADD CONSTRAINT b_exp_cat_fk FOREIGN KEY ( budget )
-        REFERENCES budgets ( b_id );
+  ADD CONSTRAINT b_exp_cat_fk FOREIGN KEY ( budget )
+    REFERENCES budgets ( b_id )
+    ON DELETE CASCADE;
 
 ALTER TABLE budgets
-    ADD CONSTRAINT b_cat_b_fk FOREIGN KEY ( b_cat )
-        REFERENCES budget_categories ( b_cat_id );
+  ADD CONSTRAINT b_cat_b_fk FOREIGN KEY ( b_cat )
+    REFERENCES budget_categories ( b_cat_id )
+    ON DELETE CASCADE;
 
-ALTER TABLE incomes
-    ADD CONSTRAINT inc_cat_inc_fk FOREIGN KEY ( inc_cat )
-        REFERENCES income_categories ( inc_cat_id );
-
-ALTER TABLE expenses
-    ADD CONSTRAINT exp_cat_exp_fk FOREIGN KEY ( exp_cat )
-        REFERENCES expense_categories ( exp_cat_id );
-
-ALTER TABLE incomes
-    ADD CONSTRAINT acc_inc_fk FOREIGN KEY ( account )
-        REFERENCES accounts ( acc_id );
+ALTER TABLE expense_categories
+  ADD CONSTRAINT acc_exp_cat_fk FOREIGN KEY ( account )
+    REFERENCES accounts ( acc_id )
+    ON DELETE SET NULL;
 
 ALTER TABLE accounts
-    ADD CONSTRAINT acc_types_acc_fk FOREIGN KEY ( acc_type )
-        REFERENCES account_types ( acc_type_id );
+  ADD CONSTRAINT acc_types_acc_fk FOREIGN KEY ( acc_type )
+    REFERENCES account_types ( acc_type_id )
+    ON DELETE CASCADE;
+
+ALTER TABLE account_balances
+  ADD CONSTRAINT acc_acc_bal_fk FOREIGN KEY ( account )
+    REFERENCES accounts ( acc_id )
+    ON DELETE CASCADE;
+
+ALTER TABLE incomes
+  ADD CONSTRAINT inc_cat_inc_fk FOREIGN KEY ( income_category )
+    REFERENCES income_categories ( inc_cat_id )
+    ON DELETE CASCADE;
+
+ALTER TABLE incomes
+  ADD CONSTRAINT acc_inc_fk FOREIGN KEY ( account )
+    REFERENCES accounts ( acc_id )
+    ON DELETE CASCADE;
