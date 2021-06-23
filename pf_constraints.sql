20bf4a3b9bfd8093b0ef293bbb057bc02d823439
elangaar <mikolaj.kowal@yahoo.pl>
2021-06-23 21:44:38 +0200

    Change the data model

--- pf_constraints.sql
+++ pf_constraints.sql
@@ -1,7 +1,12 @@
 ALTER TABLE expenses
-  ADD CONSTRAINT exp_cat_exp_fk FOREIGN KEY ( exp_cat )
+  ADD CONSTRAINT exp_cat_exp_fk FOREIGN KEY ( expense_category )
     REFERENCES expense_categories ( exp_cat_id )
     ON DELETE CASCADE;
+
+ALTER TABLE expenses
+  ADD CONSTRAINT acc_exp_fk FOREIGN KEY ( account )
+    REFERENCES accounts ( acc_id )
+    ON DELETE SET NULL;
 
 ALTER TABLE expense_categories
   ADD CONSTRAINT b_exp_cat_fk FOREIGN KEY ( budget )
@@ -9,23 +14,14 @@ ALTER TABLE expense_categories
     ON DELETE CASCADE;
 
 ALTER TABLE budgets
-  ADD CONSTRAINT b_cat_b_fk FOREIGN KEY ( b_cat )
+  ADD CONSTRAINT b_cat_b_fk FOREIGN KEY ( budget_category )
     REFERENCES budget_categories ( b_cat_id )
     ON DELETE CASCADE;
 
-ALTER TABLE expense_categories
-  ADD CONSTRAINT acc_exp_cat_fk FOREIGN KEY ( account )
-    REFERENCES accounts ( acc_id )
-    ON DELETE SET NULL;
 
 ALTER TABLE accounts
-  ADD CONSTRAINT acc_types_acc_fk FOREIGN KEY ( acc_type )
+  ADD CONSTRAINT acc_types_acc_fk FOREIGN KEY ( account_type )
     REFERENCES account_types ( acc_type_id )
-    ON DELETE CASCADE;
-
-ALTER TABLE account_balances
-  ADD CONSTRAINT acc_acc_bal_fk FOREIGN KEY ( account )
-    REFERENCES accounts ( acc_id )
     ON DELETE CASCADE;
 
 ALTER TABLE incomes
